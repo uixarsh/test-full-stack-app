220d6fccb796b50262a15b4c25fb0c2480a10dde
Arsh Singhal <arsh.singhal@drishinfo.com>
2025-04-05 14:30:14 +0530

    Updating UI

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -8,10 +8,11 @@ interface Card{
 
 const CardComponent: React.FC<{card : Card}> = ({card}) => {
     return (
-        <div className="card">
-            <h1>{card.username}</h1>
-            <p>{card.email}</p>
-        </div>
+            <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-100">
+                <div className="text=sm text-gray-600"> Id: {card.id}</div>
+                <div className="text-lg font-semibold text-gray-800"> {card.username}</div>
+                <div className="text-md text-gray-700"> {card.email}</div>
+            </div>
     )
 };
 
--- frontend/src/components/UserInterace.tsx
+++ frontend/src/components/UserInterace.tsx
@@ -14,10 +14,44 @@ interface UserInterfaceProps{
 
 const UserInterface: React.FC<UserInterfaceProps> = ({backendName}) => {
 
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+    const [users, setUsers] = useState<User[]>([]);
+    const [newUser, setNewUser] = useState({id: '', name: '', email: ''})
+    const [updatedUser, setUpdatedUser] = useState({id: '', name: '', email: ''})
+
+    const backgroundColors: { [key: string]: string } = {
+        flask: 'bg-blue-500',
+    };
+
+    const buttonColors: { [key: string]: string } = {
+        flask: 'bg-blue-700 hover:bg-blue-600',
+    };
+
+    const bgColor = backgroundColors[backendName as keyof typeof backgroundColors] || 'bg-gray-200';
+    const btnColor = buttonColors[backendName as keyof typeof buttonColors] || 'bg-gray-500 hover:bg-gray-600';
+
+
+    // Fetch users from backend
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await axios.get(`${apiUrl}/api/${backendName}/users`);
+                setUsers(response.data.reverse());
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+        fetchData();
+    }, [backendName, apiUrl]);
+
+    // Add user
+    
+
+
     return (
-        <div>
-            <h1>{backendName}</h1>
-            <CardComponent card={{id:1, username: 'John Doe', email: 'john.doe@example.com'}} />
+        <div className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
+            <img src={`/${backendName}logo.svg`} alt={`${backendName} Logo`} className="w-20 h-20 mb-6 mx-auto" />
+            <h2 className="text-xl font-bold text-center text-white mb-6">{`${backendName.charAt(0).toUpperCase() + backendName.slice(1)} Backend`}</h2>
         </div>
     );
 }
